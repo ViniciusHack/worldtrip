d91956f4901b0a05727b7dea45d243a8ec3e27b2
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-04-03 23:44:06 -0300

    feature: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Flex, Image } from "@chakra-ui/react";
+
+
+export function Header() {
+  return (
+    <Flex w="100vw" h="100" align="center" justify="center">
+      <Image src="/images/logo.svg" alt="Logo worldtrip avião entre a palavra worldtrip"/>
+    </Flex>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import '../styles/globals.css'
+import { Header } from '../components/Header'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
     </ChakraProvider>
   )
